4ae8033d14e1d272abc6df05c07975bcca6e2738
NeutronSpaceAgency <101946066+spacesaicodes1234@users.noreply.github.com>
2022-11-21 21:31:43 +0400

    Update function.js

--- function.js
+++ function.js
@@ -18,23 +18,31 @@ function myFunction() {
 }
 
 function showHide() {
+  var y = document.getElementById("myInput");
   var x = document.getElementById("myDIV");
+  var z = document.getElementById("mob");
+  z.textContent = "Hide More Items ⬆️"
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+    z.textContent = "Show More items ⬇️"
+  }
 }
+
 
 function sh1() {
   var x = document.getElementById("myDIV");
   x.style.display = "none";
+  var y = document.getElementById("myInput");
+  y.style.display = "none";
 }
 
-function hideItems() {
-  var x = document.getElementById("myDIV");
-  x.style.display = "none";
-}
 
 
 document.getElementById('add-to-list').onclick = function() {
